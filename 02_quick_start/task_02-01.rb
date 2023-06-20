b10ea166375e56ed73f14bf31357ec9643039527
DenRebrov <den_rebrov@list.ru>
2023-06-20 21:55:13 +0300

    Task 02 Done

---
+++ 02_quick_start/task_02-01.rb
@@ -0,0 +1,11 @@
+class User; end
+class Computer; end
+class Net; end
+class Host; end
+
+user = User.new
+computer = Computer.new
+net = Net.new
+host = Host.new
+
+puts(user, computer, net, host)
---
+++ 02_quick_start/task_02-02.rb
@@ -0,0 +1,13 @@
+class User
+  def fio
+  	'Денис Ребров'
+  end
+
+  def profession
+  	'Инженер-программист'
+  end
+end
+
+user = User.new
+
+puts(user.fio, user.profession)
